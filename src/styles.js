7b3897799715d14ad902ade2fe5cfb9adb4d54f1
Nedu Robert <nedumrobert@gmail.com>
2018-05-09 14:19:45 +0100

    added styling

---
+++ src/styles.js
@@ -0,0 +1,29 @@
+const Container = {
+    display: "flex",
+    justifyContent: "center",
+    maxWidth: "450px",
+    width: "100%",
+    margin: "3em auto",
+};
+
+const HeaderContainer = {
+    marginBottom: "2em",
+};
+
+const Header = {
+    fontSize: "24px",
+    color: "#FFF",
+};
+
+const Rule = {
+    width: "2em",
+    border: "0.1em solid #7ab55c",
+    margin: 0,
+};
+
+export const styles = {
+    Container,
+    HeaderContainer,
+    Header,
+    Rule,
+};
